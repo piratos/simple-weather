450d7235804ba3909bdde1fb45aaae8716bf9433
piratos <sayf.piratos@gmail.com>
2019-05-01 18:17:01 +0200

    Update the Gui

--- weatherapp.py
+++ weatherapp.py
@@ -8,6 +8,7 @@ from kivy.uix.label import Label
 
 import pyowm
 
+# Use your free API key from openweathermap
 API_KEY = "72d1868964a13571511aad0ba972659c"
 owm = pyowm.OWM(API_KEY)
 
@@ -19,17 +20,20 @@ class LabelWithBackground(Label):
 class WeatherScreen(BoxLayout):
 	def reload(self):
 		res = self.conditions()
-		self.ids.wind.text = res.get("wind")
-		self.ids.hum.text = res.get("hum")
-		self.ids.temp.text = res.get("temp")
+		self.ids.wind.text = "Wind: {}".format(res.get("wind"))
+		self.ids.hum.text = "Hum: {}".format(res.get("hum"))
+		self.ids.temp_min.text = "Min: {}°C".format(res.get("temp")[0])
+		self.ids.temp_now.text = "Now: {}°C".format(res.get("temp")[1])
+		self.ids.temp_max.text = "Max: {}°C".format(res.get("temp")[2])
 
 	def conditions(self):
-		observation = owm.weather_at_place('London,GB')
+		# Change this to your city
+		observation = owm.weather_at_place('Clichy,FR')
 		w = observation.get_weather()
 		tmp = w.get_temperature('celsius')
 		wind = "{} miles/h".format(w.get_wind().get('speed'))
 		hum = "{} %".format(w.get_humidity())
-		temp = "Min: {}°C \nNow: {}°C \nMax: {}°C".format(
+		temp = (
 			tmp.get('temp_min'),
 			tmp.get('temp'),
 			tmp.get('temp_max')
